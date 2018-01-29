fcdf7de2e25a121b90a598ef89961aec6d3862b7
nburner <nburner@harding.edu>
2018-01-29 01:37:37 -0600

    Initial Commit

---
+++ Assets/CubeSpawner.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeSpawner : MonoBehaviour {
+
+    public GameObject cubePrefabVar;
+    public float timer = 0.5f;
+
+// Use this for initialization
+    void Start ()
+    {
+        InvokeRepeating("Spawn", 0.0f, 0.5f);
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        
+    }
+
+    void Spawn()
+    {
+        // Random position help: https://answers.unity.com/questions/759219/spawn-a-gameobject-in-random-position-within-an-ar.html
+        // Random color help: https://docs.unity3d.com/ScriptReference/Random.ColorHSV.html        
+        Vector3 position = new Vector3(Random.Range(-5.0F, 0.0F), 5, Random.Range(-5.0F, 5.0F));
+        var go = Instantiate(cubePrefabVar, position, Quaternion.identity);
+        go.GetComponent<Renderer>().material.color = Random.ColorHSV();
+        Destroy(go, 5.0f);
+    }
+}
---
+++ Assets/GameObjecDestroyer.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameObjecDestroyer : MonoBehaviour {
+
+    private Ray ray;
+    private RaycastHit hit;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        // Click to destroy object help: https://answers.unity.com/questions/444374/destroy-a-single-prefab-with-a-click-.html
+        ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        if (Physics.Raycast(ray, out hit, 1000.0f) && Input.GetMouseButtonDown(0))
+        {
+            if (hit.collider.gameObject.tag != "Ignore")
+            {
+                Destroy(hit.collider.gameObject);
+            }
+        }
+    }
+}
